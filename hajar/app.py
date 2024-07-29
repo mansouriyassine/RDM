4b0eb19bf1ccb2f09145553d26f29da7eafe7f48
mansouriyassine <mansouri.yassine1@gmail.com>
2024-07-29 18:37:26 +0100

    0

---
+++ hajar/app.py
@@ -0,0 +1,24 @@
+# app.py
+from flask import Flask, render_template, jsonify
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/surprise')
+def surprise():
+    phrases = [
+        "hajjoura",
+        "9ahbouna",
+        "boha 3anda tabbouna",
+        "za3kouna",
+        "bangola",
+        "mangola"
+    ]
+    return jsonify({'phrase': random.choice(phrases)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
